5818e22a2dcd89ce4ea117c4e42ed8cff4241be9
roar sunde <roar.sunde@gmail.com>
2022-02-27 11:00:32 +0100

    initial

---
+++ src/DataStructures/Applicator.fsx
@@ -0,0 +1,69 @@
+﻿#r "nuget: FSharpPlus"
+
+open FSharpPlus;
+
+
+let whenSomeandThenSome a b = 
+    match a with 
+    | Some a -> 
+        match b with 
+        | Some b -> Some ( a + b )
+        | None -> None
+    | None -> None
+
+
+
+(Some 2, None)
+||> whenSomeandThenSome
+
+let m = [("email","ahahah"); ("postNo","noway")]|> Map.ofSeq
+
+
+type Validation<'T,'E> = 
+    | Ok of 'T
+    | Fail of 'E
+
+type Error = 
+    | ValidationError
+
+let validateEmail (m:Map<string,string>) = 
+    match m.ContainsKey "email" with 
+    | true -> Ok m
+    | false -> Fail  "email error"
+
+let validatePostNo (m:Map<string,string>) = 
+    match m.ContainsKey "postNo2" with 
+    | true -> Ok m
+    | false -> Fail  "postNoError"
+
+// previous was either ok or failure
+
+let validate f = 
+    // returns a function that takes a previous validation v
+    // that executes f and returns either a Ok or Fail depending on v
+    // takes to args previous validation and current
+    // Should return a tuple right or how do we now when 
+    // valueToProcess, previousValidation<Ok, Fail>
+    (fun (vtp, pv) -> 
+        match pv with
+        | Ok m -> 
+            let q = f m
+            match q with 
+            | Ok _ -> (q, Ok m)
+            | Fail l -> (q, Ok m)
+        | Fail _ ->  
+            let q = f m
+            match q with 
+            | Ok m -> (q, Ok m)
+            | Fail _  ->  (q, Ok m)
+    )
+
+
+
+//let q =  
+//    m
+//    // I have a map and i validateTheEmail
+//    // So first is just either a Failure or Ok       
+//    |!> validate validateEmail
+//    ||> validate validatePostNo
+    
---
+++ src/DataStructures/Tree.fs
@@ -0,0 +1,122 @@
+﻿namespace DataStructures
+    
+module Graph = 
+    open System
+
+    type Id = string
+    type Vertice<'V>   = {_VerticeData:'V option; Src:Id; Dst:Id; Id : Id}
+    type Node<'O>      = {_NodeData:'O option;Id: Id}
+
+    type Graph<'V,'O>  = {Nodes: Map<Id,Node<'O>>; Vertices: Map<Id,Vertice<'V>>}    
+    
+    let id () = Guid.NewGuid().ToString()
+
+    let makeGraph<'O,'V > = {Nodes = Map.empty<Id,Node<'O>> ; Vertices = Map.empty<Id,Vertice<'V>>}
+
+    let stringToNode s =
+        {Id = s ; _NodeData = None}
+
+    let applyObjectToGraph graph object : Graph<'a,'b> = 
+        {graph with Nodes = Map.add object.Id object graph.Nodes}
+
+    let nodesToVertice a b =
+        {_VerticeData = None;Src = a.Id ; Dst =  b.Id ; Id = id()}
+
+    let applyVerticeToGraph  graph (vertice:Vertice<'a>) : Graph<'a,'b> = 
+        {graph with Vertices = Map.add (vertice.Id) vertice (graph.Vertices)}
+
+    let findObjectById Id graph = 
+        if Map.containsKey Id graph.Nodes then
+            Map.find Id graph.Nodes
+        else            
+            failwithf "Could not locate Object with Id: %s in graph" Id
+
+    let stringToNodeToGraph  snode graph = 
+        stringToNode snode    
+        |> applyObjectToGraph graph
+
+    let connectObjectsById a b graph = 
+        Tuple.Create(
+            (findObjectById a graph), 
+            (findObjectById b graph)
+        )
+        ||> nodesToVertice  
+        |> applyVerticeToGraph graph
+
+    let verticeFilter f g =         
+        Map.toList g.Vertices
+        |> List.filter (fun (k,v) -> f v) 
+        |> List.map (fun (k,v) -> v)
+         
+    
+    let getDestinationsFromObjectId id graph = 
+        graph
+        |> verticeFilter (fun v -> v.Src = id)
+        |> List.map (fun v -> v.Dst)
+        
+
+    let resolveObjectById id graph = 
+        let o = findObjectById id graph 
+        let initIds = getDestinationsFromObjectId o.Id graph
+        
+        let rec loop vn vo  = 
+        
+            match vn with 
+            |(f :: r) ->                 
+                
+                let nvlst = 
+                    getDestinationsFromObjectId f graph
+                    |> Set.ofList
+                    |> (fun nlst -> Set.difference nlst (Set.ofList vo))         
+                    |> Set.toList
+                
+                if nvlst.IsEmpty then    
+                    loop r (f :: vo )
+                else 
+                    let r = List.append nvlst r |> Seq.ofList |> Seq.toList
+                    loop r (f :: vo)
+            | _ -> 
+                vo
+        
+        loop initIds []
+
+    let resolveAll graph = 
+
+        let nodes = 
+            List.map (fun (k,v) -> 
+                    let oId = v.Id
+                    let ores = resolveObjectById oId graph
+                    (oId, ores)
+                )                 
+                (Map.toList graph.Nodes)
+
+        Map.ofList nodes
+
+
+//open Graph
+
+//let graph = 
+//    let pr g = 
+//        printfn "%A" g
+//        g
+
+//    makeGraph<string,string>
+//    |> stringToNodeToGraph "groupA"
+//    |> stringToNodeToGraph "groupB" 
+//    |> stringToNodeToGraph "groupC" 
+//    |> stringToNodeToGraph "groupD" 
+//    |> connectObjectsById "groupA" "groupB" 
+//    |> connectObjectsById "groupA" "groupC" 
+//    |> connectObjectsById "groupC" "groupD" 
+
+//let x = resolveObjectById "groupA" graph
+//let y = resolveAll graph
+
+
+
+
+
+
+    
+
+
